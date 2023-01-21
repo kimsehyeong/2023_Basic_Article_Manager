4d0ff815870e2693b4c284cd3655472a1ca04164
kimsehyeong <lego5542@naver.com>
2023-01-21 23:24:40 +0900

    명령어 입력

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,6 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		System.out.println("===프로그램 시작===");
-		
-		Scanner sc = new Scanner(System.in);
-		
-		String cmd = sc.nextLine(); //입력기능.
-		
 		/**
 		 * nextLine = String 형 
 		 * nextInt = int형
@@ -18,7 +11,22 @@ public class Main {
 		 * 에러 : 실행햇는데 완전히 안되는거
 		 * 예외 : 실행했는데 해보니깐 안됌  -> "필수"
 		 */
-		System.out.println("입력된 명령어 : " + cmd);
+		
+		
+		System.out.println("===프로그램 시작===");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		//while -> 무한 루프
+		while(true) {
+			System.out.printf("명령어) :");
+			String cmd = sc.nextLine(); //입력기능.
+			
+			if(cmd.equals("exit")) {
+				
+				break; // : 무한루프 종료
+			}
+		}
 		
 		
 		System.out.println("===프로그램 끝===");
