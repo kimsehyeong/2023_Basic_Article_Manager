47a9703b0d82d03bd3005dc5a4a36a9ba51fb401
kimsehyeong <lego5542@naver.com>
2023-01-22 11:27:58 +0900

    게시물 작성 기능 구현

--- src/Main.java
+++ src/Main.java
@@ -14,16 +14,44 @@ public class Main {
 		
 		
 		System.out.println("===프로그램 시작===");
-		
 		Scanner sc = new Scanner(System.in);
+		int lastArticleID = 0;
 		
 		//while -> 무한 루프
 		while(true) {
+			
+			
 			System.out.printf("명령어) :");
 			String cmd = sc.nextLine(); //입력기능.
 			
-			if(cmd.equals("exit")) {
+			if(cmd.length() == 0) {
+				System.out.println("명령어를 입력해주세요");
+				continue;
+			}
+			
+			if(cmd.equals("article write")) {
+				int id = lastArticleID+1;
+				lastArticleID = id;
 				
+				System.out.printf("제목 :");
+				String title = sc.nextLine();
+				System.out.printf("내용 :");
+				String content = sc.nextLine();
+				
+//				lastArticleID++;
+				
+				System.out.printf("%d글이 생성되었습니다\n",id);
+				
+//				System.out.printf("%s, %s\n",title,content);
+			}else if(cmd.equals("article list")) {
+					System.out.println("게시글이 없습니다");
+				}
+			else {
+				System.out.println("존재하지 않는 명령어 입니다.");
+			}
+
+			
+			if(cmd.equals("exit")) {
 				break; // : 무한루프 종료
 			}
 		}
