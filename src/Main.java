eb1790f2a36876a213e96009b36dd1503befcb3d
sehyeong Kim <lego5542@naver.com>
2023-01-29 13:28:00 +0900

    게시물 리스트 구현

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,6 @@ public class Main {
 	public static void main(String[] args) {
 		/**
 		 * nextLine = String 형 nextInt = int형
-		 * text is hard......
 		 * 에러 : 실행햇는데 완전히 안되는거 예외 : 실행했는데 해보니깐 안됌 -> "필수"
 		 */
 
@@ -29,6 +28,8 @@ public class Main {
 				System.out.println("명령어를 입력해주세요");
 				continue;
 			}
+			
+			
 
 			if (cmd.equals("article write")) {
 				int id = lastArticleID + 1;
@@ -41,16 +42,34 @@ public class Main {
 //				lastArticleID++;
 
 				Article article = new Article(id, title, content);
-
+				articles.add(article);
 				System.out.printf("%d글이 생성되었습니다\n", id);
 
 //				System.out.printf("%s, %s\n",title,content);
-			} else if (cmd.equals("article list")) {
-				System.out.println("게시글이 없습니다");
-			} else {
+			}else if (cmd.equals("article list")) {
+				System.out.println("번호	|	제목");
+				/**
+				 * 배열 , list를 역순 조회 할때는
+				 * 초기값에 size() -1해주고 종료조건을 0이 될때까지 i--해준다
+				 */
+				
+				for(int i = articles.size() - 1; i>=0; i--) {
+					Article article = articles.get(i);
+					System.out.printf("%d	|	%s\n",article.id,article.title);
+				}
+				
+				if(articles.size() == 0){
+					System.out.println("게시물이 없습니다");
+					continue;
+				}
+			} 
+			
+			else {
 				System.out.println("존재하지 않는 명령어 입니다.");
 			}
 
+			
+			
 			if (cmd.equals("exit")) {
 				break; // : 무한루프 종료
 			}
